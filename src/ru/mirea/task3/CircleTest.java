8face085603c36e57b0b3165ff442035a753e82a
Max Fetisov <78416248+MaxNard@users.noreply.github.com>
2021-09-22 16:44:05 +0300

    Update and rename CircleTest (1).java to CircleTest.java

--- src/ru/mirea/task3/CircleTest.java
+++ src/ru/mirea/task3/CircleTest.java
@@ -5,14 +5,5 @@
     c1.setRadius(50);
 
     System.out.print("The radius of the circle is "+c1.getRadius());
-
-    ///
-
-    Human h1 = new Human();
-    h1.setName("Denis");
-    h1.setAge(12);
-
-    System.out.print("\nHuman name is "+h1.getName()+". Human age is "+h1.getAge()+".");
-    
    }
   }
