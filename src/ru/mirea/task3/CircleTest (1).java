84f606dd61a70800f45be6140462dcc488f941bc
Max Fetisov <78416248+MaxNard@users.noreply.github.com>
2021-09-22 16:42:50 +0300

    Add files via upload

---
+++ src/ru/mirea/task3/CircleTest (1).java
@@ -0,0 +1,18 @@
+  public class CircleTest  {
+   public static void main(String[] args) {
+    Circle c1 = new Circle();
+
+    c1.setRadius(50);
+
+    System.out.print("The radius of the circle is "+c1.getRadius());
+
+    ///
+
+    Human h1 = new Human();
+    h1.setName("Denis");
+    h1.setAge(12);
+
+    System.out.print("\nHuman name is "+h1.getName()+". Human age is "+h1.getAge()+".");
+    
+   }
+  }
