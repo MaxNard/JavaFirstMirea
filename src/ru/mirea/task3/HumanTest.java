db89b2e8cea872c80c9927fed0daa49b7fa87c50
Max Fetisov <78416248+MaxNard@users.noreply.github.com>
2021-09-22 16:49:40 +0300

    Add files via upload

---
+++ src/ru/mirea/task3/HumanTest.java
@@ -0,0 +1,9 @@
+  public class HumanTest  {
+   public static void main(String[] args) {
+    Human h1 = new Human();
+    h1.setName("Denis");
+    h1.setAge(12);
+
+    System.out.print("\nHuman name is "+h1.getName()+". Human age is "+h1.getAge()+".");
+   }
+  }
