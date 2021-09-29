f67f83b5d96451e5c98b0f9aae7e3c757e7b6065
Max Fetisov <78416248+MaxNard@users.noreply.github.com>
2021-09-29 16:38:37 +0300

    Add files via upload

---
+++ src/ru/mirea/task7/Circle.java
@@ -0,0 +1,33 @@
+public class Circle extends Shape {
+  protected double radius;
+
+  public Circle(){}
+  public Circle (double radius){
+    this.radius = radius;
+  }
+  public Circle (double radius, String color, boolean filled){
+    this.radius = radius;
+    this.color = color;
+    this.filled = filled;
+  }
+
+  public double getRadius(){
+    return radius;
+  }
+
+  public void setRadius (double radius){
+    this.radius = radius;
+  }
+
+  public double getArea(){
+    return Math.PI*Math.pow(radius,2);
+  }
+
+  public double getPerimeter(){
+    return Math.PI*2*radius;
+  }
+
+  public String toString(){
+    return getColor()+" круг с радиусом: "+getRadius()+". Длина периметра: "+getPerimeter()+". Площадь: "+getArea();
+  }
+}
---
+++ src/ru/mirea/task7/Square.java
@@ -0,0 +1,31 @@
+public class Square extends Rectangle{
+  public Square(){};
+  public Square (double side){
+    length = side;
+  }
+  public Square (double side, String color,boolean filled){
+    length = side;
+    this.color = color;
+    this.filled = filled;
+  }
+
+  public double getSide(){
+    return length;
+  }
+
+  public void setSide(double side){
+    length = side;
+  }
+
+  public void setWidth (double side) {
+    this.width=side;
+  }
+
+  public void setLength (double side) {
+    this.length=side;
+  }
+
+  public String toString(){
+    return getColor()+" квадрат со сторонами: "+getSide()+". Длина периметра: "+getSide()*4+". Площадь: "+Math.pow(getSide(),2);
+  }
+}
