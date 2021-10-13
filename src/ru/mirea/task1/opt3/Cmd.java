a77053a9e202917bb3da5799cee2f4cdff86f79a
Max Fetisov <78416248+MaxNard@users.noreply.github.com>
2021-10-13 19:29:13 +0300

    Update Cmd.java

--- src/ru/mirea/task1/opt3/Cmd.java
+++ src/ru/mirea/task1/opt3/Cmd.java
@@ -2,7 +2,7 @@ package ru.mirea.task1.opt3;
 
 public class Cmd {
     public static void main(String[] args) {
-        if (args.length==0) {
+        if (args.length>0) {
             for (int i = 0; i < args.length; i++) {
                 System.out.print(args);
             }
